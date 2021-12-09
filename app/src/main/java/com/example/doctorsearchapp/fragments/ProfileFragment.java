10ffcfed64882704223187b409470914fa123be1
Adlai M.B <adlaimb@gmail.com>
2021-12-08 22:03:16 -0800

    bind data to profile fragement

--- app/src/main/java/com/example/doctorsearchapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/doctorsearchapp/fragments/ProfileFragment.java
@@ -10,18 +10,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.doctorsearchapp.R;
+import com.example.doctorsearchapp.models.Users;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ProfileFragment extends Fragment {
 
     private static final String TAG = "ProfileFragment";
     private ImageView ivProfileImage;
+    private TextView tvUsername;
+    private TextView tvUserId;
+    private Users currUser;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -38,5 +42,18 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ivProfileImage = view.findViewById(R.id.ivProfileImage);
+        tvUsername = view.findViewById(R.id.tvUsername);
+        tvUserId = view.findViewById(R.id.tvUserId);
+
+        currUser = new Users(ParseUser.getCurrentUser());
+        ParseFile profileImage = currUser.getProfilePic();
+
+        tvUsername.setText(currUser.getUsername());
+        tvUserId.setText(currUser.getProfileCreation());
+
+        if (profileImage != null)
+        {
+            Glide.with(getContext()).load(profileImage.getUrl()).into(ivProfileImage);
+        }
     }
 }
